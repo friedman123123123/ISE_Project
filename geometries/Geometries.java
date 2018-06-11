8f1b42ab3a47678be5820d2967f4b420b3f95bce
danb7 <38322899+danb7@users.noreply.github.com>
2018-06-11 19:44:44 +0300

    nofix

--- geometries/Geometries.java
+++ geometries/Geometries.java
@@ -26,6 +26,7 @@ public class Geometries extends Geometry {
 	}
 
 	
+	
 	@Override
 	public Map<Geometry, List<Point3D>> findIntersectionPoints(Ray r) {
 		findIntersections = new HashMap<Geometry, List<Point3D>>();
